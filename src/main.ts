6e92ca7e683f3befa4a9c94718b64399e68a773f
Fledarath <frederic.ledarath@epitech.eu>
2024-05-22 18:45:45 +0200

    configure values merging

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,66 @@
 import * as core from '@actions/core'
 import { wait } from './wait'
+import * as github from '@actions/github'
+import fs from 'fs/promises'
+import Mustache from 'mustache'
 
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 export async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
+  const context = github.context
+  const valueFiles = getValueFiles([
+    './.github/config/pr.yml',
+    './.github/config/_common.yml'
+  ])
+  const secrets = getSecrets(core.getInput('secrets'))
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+  await renderFiles(valueFiles.concat(['./values.yml']), {
+    secrets,
+    deployment: context.payload.deployment
+  })
+}
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+function renderFiles(files: string[], data: Record<string, any>) {
+  core.debug(
+    `rendering value files [${files.join(',')}] with: ${JSON.stringify(data)}`
+  )
+  const tags: [string, string] = ['${{', '}}']
+  const promises = files.map(async file => {
+    const content = await fs.readFile(file, { encoding: 'utf8' })
+    const rendered = Mustache.render(content, data, {}, tags)
+    await fs.writeFile(file, rendered)
+  })
+  return Promise.all(promises)
+}
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
+function getSecrets(secrets: string) {
+  if (typeof secrets === 'string') {
+    try {
+      return JSON.parse(secrets)
+    } catch (err) {
+      return secrets
+    }
   }
+  return secrets
+}
+
+function getValueFiles(files: string[]) {
+  let fileList
+
+  if (typeof files === 'string') {
+    try {
+      fileList = JSON.parse(files)
+    } catch (err) {
+      // Assume it's a single string.
+      fileList = [files]
+    }
+  } else {
+    fileList = files
+  }
+  if (!Array.isArray(fileList)) {
+    return []
+  }
+  return fileList.filter(f => !!f)
 }
